d2fef59782dade73c2698e1b6fdf9a571a9738f2
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-10-02 11:13:30 -0500

    sales figures

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
 // Event listener that fires when a user clicks a button.
 document.getElementById("myBtn").addEventListener("click", function(){
-  computeMPG();
+  computeFigures();
 });
 
 // Global variables
-var miles;      // To hold miles driven
-var gallons;    // To hold gallons used
-var mpg;        // To hold MPG
+var day1, day2, day3, total;
 
-function computeMPG(){
-  // Get the miles driven and assign it to the miles variable.
-  miles = parseFloat(document.getElementById('miles').value);
+function computeFigures(){
+  // Get the sales for each day
+  day1 = parseFloat(document.getElementById('day1').value);
+  day2 = parseFloat(document.getElementById('day2').value);
+  day3 = parseFloat(document.getElementById('day3').value);
 
-  // Get the gallons used and assign it to the gallons variable.
-  gallons =  parseFloat(document.getElementById('gallons').value);
-
-  // Validate data entered
-  if (miles === null || miles === "" || isNaN(miles)) {
+  // Display
+  if (day1 === null || day1 === "" || isNaN(day1)) {
     // Display an error message for miles
-    document.getElementById('outMPG').innerHTML = "Miles must be filled out!";
+    document.getElementById('outTotal').innerHTML = "Day 1 must be filled out!";
+  } else if (day2 === null || day2 === "" || isNaN(day2)) {
+      // Display an error message for gallons
+      document.getElementById('outTotal').innerHTML = "Day 2 must be filled out!";
+  } else if (day2 === null || day2 === "" || isNaN(day2)) {
+      // Display an error message for gallons
+      document.getElementById('outTotal').innerHTML = "Day 3 must be filled out!";
+  } else {
+      // Calculate total sales
+      total = day1 + day2 + day3;
   }
-  else if (gallons === null || gallons === "" || isNaN(gallons)) {
-    // Display an error message for gallons
-    document.getElementById('outMPG').innerHTML = "Gallons must be filled out!";
-  }
-  else {
-    // Calculate MPG
-    var mpg = miles / gallons;
-
-    // Display MPG
-    document.getElementById('outMPG').innerHTML = mpg.toFixed(2);
-  }
+  // Display the sales figures
+  document.getElementById('outTotal').innerHTML = "Day 1: " + day1.toFixed(2);
+  document.getElementById('outTotal').innerHTML = "Day 2: " + day2.toFixed(2);
+  document.getElementById('outTotal').innerHTML = "Day 3: " + day3.toFixed(2);
+  document.getElementById('outTotal').innerHTML = "Total: " + total.toFixed(2);
 }
